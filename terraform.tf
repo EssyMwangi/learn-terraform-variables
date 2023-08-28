1b6a35bb9b7aba4ba99319f376909af28767369a
Essy Muthoni Mwangi <sonnieessy@gmail.com>
2023-08-28 15:35:04 +0300

    remove terraform.tfvar file

--- terraform.tf
+++ terraform.tf
@@ -3,20 +3,20 @@
 
 terraform {
 
-  /* Uncomment this block to use Terraform Cloud for this tutorial
+  
   cloud {
-      organization = "organization-name"
+      organization = "learn-terraform-variables-ess"
       workspaces {
         name = "learn-terraform-variables"
       }
   }
-  */
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.15.0"
     }
   }
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.5.0"
 }
 
