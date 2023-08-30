e1fe328fe5254544adbb6d94bbf636dd7568ab01
Essy Muthoni Mwangi <sonnieessy@gmail.com>
2023-08-30 15:47:28 +0300

    separated app and data subnets for private subnets

--- main.tf
+++ main.tf
@@ -18,7 +18,8 @@ module "vpc" {
   cidr = var.vpc_cidr_block
 
   azs             = data.aws_availability_zones.available.names
-  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  app_private_subnets = slice(var.app_private_subnet_cidr_blocks, 0, var.app_private_subnet_count)
+  data_private_subnets = slice(var.data_private_subnet_cidr_blocks, 0, var.data_private_subnet_count)
   public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
 
   enable_nat_gateway = true
